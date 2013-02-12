ad93ad71460935e96be8c0c3c7e44f7f287090f4
Tom Hosford <tom.hosford@gmail.com>
2013-02-11 20:18:12 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnitchyServer::Application.config.secret_token = '3b16a43c7d1fe91ca2a0c2a6b3b05b87f55622bfe6de88cf2d0d1a7ea5ea1a222a0a4514837e156e3c5658c9a331c1dcb11a32bf6824e458e1ccb71e35190538'
